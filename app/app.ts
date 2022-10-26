d0d737ad4df23eda501fab3537669115a14efd7d
Sabrina Drashen <sdrashen@gmail.com>
2022-10-25 21:52:52 -0300

    Configure typescript

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,17 @@
 import { Negociacao } from "./models/negociacao.js";
+
 // const negociacao = new Negociacao(new Date(), 10, 100);
 // console.log(negociacao);
 // negociacao.quantidade = 10000;
 // console.log(negociacao);
+
 /**Conseguimos alterar quantidade mas não a quantidade privada da instância de negociação.
  * O JS por ser uma linguagem dinâmica, ele add a property "quantidade" dinamicamente
  * nesse objeto. Ele não consegue ter relação nenhuma com a quantidade que está
  * internamente na classe.
  */
+
 const negociacao = new Negociacao(new Date()); //TS acuses error here.
 console.log(negociacao.volume);
+
 /**O get dá acesso como se fosse uma propriedade de classe. */
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,39 +1,62 @@
+var __classPrivateFieldSet =
+  (this && this.__classPrivateFieldSet) ||
+  function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+      throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+  };
+var __classPrivateFieldGet =
+  (this && this.__classPrivateFieldGet) ||
+  function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+      throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+  };
+var _data, _quantidade, _valor;
 export class Negociacao {
-  //Atributos privados #
-  #data;
-  #quantidade;
-  #valor;
-
   constructor(data, quantidade, valor) {
-    this.#data = data;
-    this.#quantidade = quantidade;
-    this.#valor = valor;
+    //Atributos privados #
+    _data.set(this, void 0);
+    _quantidade.set(this, void 0);
+    _valor.set(this, void 0);
+    __classPrivateFieldSet(this, _data, data);
+    __classPrivateFieldSet(this, _quantidade, quantidade);
+    __classPrivateFieldSet(this, _valor, valor);
   }
-
   /**Aqui vemos que, com a linguagem mais moderna do Js,
    * conseguimos criar atributos provados de uma classe,
    * nos quais podemos atribuir valores atavés do construtor
    * ou através de métodos acessadores.
    **/
-
   /**Assim como o método acima, o getter consegue acessar os
    * atributos privados da classe. O getter dá aceso como se fosse
    * uma propriedade de classe. Não é possível atribuir nada por ele,
    * é read only.
    */
   get data() {
-    return this.#data;
+    return __classPrivateFieldGet(this, _data);
   }
-
   get quantidade() {
-    return this.#quantidade;
+    return __classPrivateFieldGet(this, _quantidade);
   }
-
   get valor() {
-    return this.#valor;
+    return __classPrivateFieldGet(this, _valor);
   }
-
   get volume() {
-    return this.#quantidade * this.#valor;
+    return (
+      __classPrivateFieldGet(this, _quantidade) *
+      __classPrivateFieldGet(this, _valor)
+    );
   }
 }
+(_data = new WeakMap()),
+  (_quantidade = new WeakMap()),
+  (_valor = new WeakMap());
+
+//File gerado pelo ts
+/*O TS converteu o código escrito usando o que há de mais
+ *moderno em TS pra uma linguagem compatível com o ECMAScript6 e
+ *no ES6 ñ tempos propriedades privadas.*/
